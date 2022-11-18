101e667b5ff4e0153527f0641d7b774ff7ef9da0
marpusik <marpusik1987@gmail.com>
2022-11-18 09:16:26 +0300

    13

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import './components/smooth-scroll';
 import './components/tooltip'
 import './components/modals'
 import './components/slider'
+import './components/validation'
---
+++ src/js/components/validation.js
@@ -0,0 +1,28 @@
+const rules = [
+  {
+    ruleSelector: '.input-name',
+    rules: [
+      {
+        rule: 'minLength',
+        value: 3
+      },
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните имя!'
+      }
+    ]
+  },
+  {
+    ruleSelector: '.input-tel',
+    tel: true,
+    telError: 'Введите корректный телефон',
+    rules: [
+      {
+        rule: 'required',
+        value: true,
+        errorMessage: 'Заполните телефон!'
+      }
+    ]
+  },
+];
